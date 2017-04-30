c1848a1bc664fb40612c735fa995334fd878a59b
Yoga Vern <sean.vernon88@gmail.com>
2017-04-30 11:09:42 -0700

    Formating Changes
    Changed some formatting as per PEP8 guidelines. Indented and removed
    whitespace. Seperated lines that were too long.

--- media.py
+++ media.py
@@ -1,17 +1,25 @@
-##IMPORT THE WEBBROWSER CLASS
+# IMPORT THE WEBBROWSER CLASS
 
 import webbrowser
 
-##DEFINE WHAT WILL MAKE UP THE MOVIE CLASS AND HOW IT WILL BE ORGANIZED BELOW
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+	#  noqa
+	'''DEFINE WHAT WILL MAKE UP THE MOVIE CLASS 
+	AND HOW IT WILL BE ORGANIZED BELOW'''
 
-##BELOW DEFINES THE SHOW_TRAILER METHOD ALLOWING THE YOUTUBE LINK TO OPEN IN THE SAME WINDOW
+    def __init__(self, movie_title, movie_storyline, 
+    			 poster_image, trailer_youtube):
+
+    	self.title = movie_title
+    	self.storyline = movie_storyline
+    	self.poster_image_url = poster_image
+    	self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+    	
+    	'''BELOW DEFINES THE SHOW_TRAILER METHOD 
+    	ALLOWING THE YOUTUBE LINK TO OPEN IN THE SAME WINDOW
+    	'''
+
+    	webbrowser.open(self.trailer_youtube_url)
